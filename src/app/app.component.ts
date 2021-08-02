4a9f7c118c5e71be948714b67b6b236e3757f274
Pilip.Shchasny <c-pshchasny@neogov.net>
2021-08-02 17:48:15 +0300

    added validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,28 @@
 import {Component, OnInit} from '@angular/core'
-import {FormGroup} from "@angular/forms";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
-  form: FormGroup
+export class AppComponent implements OnInit {
+    form: FormGroup
 
-  ngOnInit(){
-    this.form = new FormGroup({})
-  }
+    ngOnInit() {
+        this.form = new FormGroup({
+            email: new FormControl('', [
+                Validators.email, Validators.required
+            ]),
+            password: new FormControl(null,[
+                Validators.required,
+                Validators.minLength(6)
+            ])
+        })
+    }
 
-  submit() {
-    console.log('fotm submited: ', this.form)
-  }
+    submit() {
+        console.log('form submited: ', this.form)
+    }
 }
 
